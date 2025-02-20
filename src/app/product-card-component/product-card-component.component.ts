81d866604361dc848fcd391d495d24975a0e32e2
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2025-02-20 07:47:55 -0800

    Building out product-card-component, product-list-component, built out product model and service.

---
+++ src/app/product-card-component/product-card-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-component',
+  imports: [],
+  templateUrl: './product-card-component.component.html',
+  styleUrl: './product-card-component.component.css'
+})
+export class ProductCardComponentComponent {
+
+}
