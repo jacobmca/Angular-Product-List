178b53483c9c0781eae48e07df3c6a62df617a73
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2025-02-21 13:33:24 -0800

    Styling added.

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -2,6 +2,6 @@ export interface Product {
     id: number;
     name: string;
     description: string;
-    price: number;
+    price: string;
     imageURL: string;
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,9 +7,9 @@ import { Product } from '../models/product.model';
 })
 export class ProductService {
   private products: Product[] = [
-    { id: 1, name: 'Product 1', description: "This is the first product", price: 1000, imageURL: 'assets/product1.jpg'},
-    { id: 2, name: 'Product 2', description: "This is the second product", price: 2000, imageURL: 'assets/product2.jpg'},
-    { id: 3, name: 'Product 3', description: "This is the third product", price: 3000, imageURL: 'assets/product3.jpg'}
+    { id: 1, name: 'Product 1', description: "This is the first product.", price: "$1,000", imageURL: '/assets/product1.jpg'},
+    { id: 2, name: 'Product 2', description: "This is the second product.", price: "$2,000", imageURL: '/assets/product2.webp'},
+    { id: 3, name: 'Product 3', description: "This is the third product.", price: "$3,000", imageURL: '/assets/product3.jpg'}
   ];
 
   getProducts(): Observable<Product[]> {
